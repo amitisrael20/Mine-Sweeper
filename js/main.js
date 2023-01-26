5c498d65297d1df704e53d3a90b8ec78ed4b6a0d
amitisrael20 <123574985+amitisrael20@users.noreply.github.com>
2023-01-26 13:58:30 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -14,37 +14,46 @@ const gCell = {
     isMine: false,
     isMarked: true
 }
-
 var gLevel = [{ SIZE: 4, MINES: 2 }, { SIZE: 5, MINES: 6 }, { SIZE: 6, MINES: 9 }]
 
 function Init() {
     console.log('Hi')
     console.log('MINE: ', MINE)
     var mineBoard = buildBoard(gLevel[0].SIZE)
-
-    console.log(mineBoard)
+    console.log('mineBoard: ', mineBoard)
     renderBoard(mineBoard)
 
+}
+var helpBoard = buildBoard(gLevel[0].SIZE)
+//console.log('helpBoard: ',helpBoard)
 
-
-
+function tempBoard(size) {
+    var tempBoard = []
+    for (var i = 0; i < size; i++) {
+        tempBoard.push([])
+        for (var j = 0; j < size; j++)
+            tempBoard[i].push([])
+    }
+    return tempBoard
 }
 
-function buildBoard(size) {
 
+function buildBoard(size) {
+    gBoard = tempBoard(size)
     const board = []
-    var idx = getRandomInt(0, size)
+    // var idx = getRandomInt(0, size)
     for (var i = 0; i < size; i++) {
         board.push([])
         for (var j = 0; j < size; j++) {
             board[i][j] = { gCell }
             if ((i === 0 && j === 2) || (i === 2 && j === 3)) {
                 board[i][j].isMine = true
-
+                //gBoard[i][j] = -1
 
             }
 
         }
+
     }
     if (size === 5) {
 
@@ -56,7 +65,7 @@ function buildBoard(size) {
         board[4][0].isMine = true
 
     }
-
+    gGame.isOn = true;
     return board
 
 }
@@ -72,14 +81,14 @@ function setMinesNegsCount(rowIdx, colIdx, board) {
         for (var j = colIdx - 1; j <= colIdx + 1; j++) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= board[i].length) continue
-            if (board[i][j] === MINE) neighborsCount++
+            if (board[i][j].isMine) neighborsCount++
         }
     }
     return neighborsCount
 }
 
 function renderBoard(board) {
-
+    var value = null
     var strHtml = '<table class="mineBoard"><tbody class="">'
     for (var i = 0; i < board.length; i++) {
         //var row = board[i]
@@ -87,24 +96,17 @@ function renderBoard(board) {
         for (var j = 0; j < board[i].length; j++) {
 
             if (!board[i][j].isMine) {
-                var value = setMinesNegsCount(i, j, board)
+                value = setMinesNegsCount(i, j, board)
                 console.log('value: ', value)
-                board[i][j] = value
+                //board[i][j] = value
 
             }
             else {
-                board[i][j] = MINE
+                value = MINE
 
             }
-            // if((i===0 && j===2) || (i===2 &&j===3)){
-            //     board[i][j]=MINE
-            // }
-            // else{
-            //     board[i][j]=setMinesNegsCount(i,j,board)
-            // }
-            //var className = (i + j) % 2 ? 'black' : 'white'
 
-            strHtml += `\t<td id="${i}-${j}- class="hide" onclick="onCellClicked(this)"><span>${board[i][j]}</span></td>\n`
+            strHtml += `\t<td id="${i}-${j}" class="cell", "hide", onclick="onCellClicked(this)"><span>${value}</span></td>\n`
         }
         strHtml += '</tr>\n'
     }
@@ -115,12 +117,39 @@ function renderBoard(board) {
 }
 
 function onCellClicked(elCell) {
-    var elGameOv = document.querySelector('gameOver')
-    var elSpan = elCell.querySelector('span')
-    if (elCell.isMine)
-        elGameOv.style.display = 'block'
+    // debugger
+    var arr = []
+    arr.push(elCell.id.split('-'))
+    var num = arr[0]
+    console.log('num: ', num)
+    if (gGame.isOn) {
+        var elGameOv = document.querySelector('.gameOver')
+        var elColor = document.getElementById(`${num[0]}-${num[1]}`)
+        var elSpan = elCell.querySelector('span')
 
-    elSpan.style.display = 'block'
+        if (elCell.textContent === MINE) {
+            elGameOv.style.display = 'block'
+            elSpan.style.display = 'block'
+            elColor.style.backgroundColor = 'darkred'
+            gGame.isOn = false
+            var elSmile = document.querySelector('.resetGame')
+            elSmile.innerText = '😭'
+            elSmile.style.backgroundColor='crimson'
+            //debugger
+            //let cells = document.getElementsByClassName('cell')
+
+        }
+
+        else {
+            //expandShown(gBoard,elCell,num[0],num[1])
+            elSpan.style.display = 'block'
+            elColor.style.backgroundColor = 'green'
+            //setMinesNegsCount(gBoard,elCell,num[i],num[j])
+            //TODO - SHOW RESTART BUTTON
+        }
+
+
+    }
 
 
 
@@ -128,16 +157,28 @@ function onCellClicked(elCell) {
 
 }
 
-function getCellCoord(strCellId) {
-    var coord = {}
-    var parts = strCellId.split('-')
-
-    coord.i = +parts[1]
-    coord.j = +parts[2]
-    return coord;
-
+function Restart() {
+    var elSmile = document.querySelector('.resetGame')
+    elSmile.innerText = '🙂'
+    elSmile.style.backgroundColor='bisque'
+    var elGameOv = document.querySelector('.gameOver')
+    elGameOv.style.display='none'
+    var mineBoard = buildBoard(gLevel[0].SIZE)
+    console.log('mineBoard: ', mineBoard)
+    renderBoard(mineBoard)
 
 }
+
+// function getCellCoord(strCellId) {
+//     var coord = {}
+//     var parts = strCellId.split('-')
+
+//     coord.i = +parts[1]
+//     coord.j = +parts[2]
+//     return coord;
+
+
+// }
 // function onCellMarked(elCell) {
 
 // }
@@ -147,12 +188,26 @@ function getCellCoord(strCellId) {
 // }
 
 function expandShown(board, elCell, i, j) {
-    var x = setMinesNegsCount(i, j, board)
-    for (var i = i; i < x; i++) {
-        var elSpan = elCell.querySelector('span')
-        elSpan.style.display = 'block'
+    // var arr = []
+    // arr.push(elCell.id.split('-'))
+    // var num = arr[0]
+    
+    for(var row=i-1; i<row+1;i++){
+        for(var col=j-1; col<j+1;col++){
+            var elColor = document.getElementById(`${row}-${col}`)
 
+        }
     }
+
+
+
+
+    // var x = setMinesNegsCount(i, j, board)
+    // for (var i = i; i < x; i++) {
+    //     var elSpan = elCell.querySelector('span')
+    //     elSpan.style.display = 'block'
+
+    // }
 
     // setMinesNegsCount(i,j,board)
 
